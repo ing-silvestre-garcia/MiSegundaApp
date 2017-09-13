277c07d129f326a77ea2301beba2946acbcad03f
Silvestre Garcia <silver@Silvestres-iMac.local>
2017-09-13 18:26:36 -0500

    Initial Commit

---
+++ Mi segunda App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mi segunda App
+//
+//  Created by Silvestre Garcia on 9/13/17.
+//  Copyright © 2017 silver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mi segunda App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mi segunda App
+//
+//  Created by Silvestre Garcia on 9/13/17.
+//  Copyright © 2017 silver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
