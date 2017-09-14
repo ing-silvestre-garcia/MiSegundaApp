8ed9268dfaf7137bf699123268d49cb884a885cd
Silvestre Garcia <silver@Silvestres-iMac.local>
2017-09-13 20:17:14 -0500

    MOD: Se agrega funcionalidad de texto dinámico y se cuenta como tarea 2

--- Mi segunda App/Home.m
+++ Mi segunda App/Home.m
@@ -7,7 +7,7 @@
 //
 
 #import "Home.h"
-
+int x=1;
 @interface Home ()
 
 @end
@@ -19,7 +19,39 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 - (IBAction)changeButtonPressed:(id)sender {
-    self.lblWelcome.text = @"Adios Mundo";
+    switch (x) {
+        case 1:
+            self.lblWelcome.text = @"Uno";
+            self.lblWelcome.textColor=[UIColor blueColor];
+            break;
+        case 2:
+            self.lblWelcome.text = @"Dos";
+            self.lblWelcome.textColor=[UIColor greenColor];
+            break;
+        case 3:
+            self.lblWelcome.text = @"Tres";
+            self.lblWelcome.textColor=[UIColor redColor];
+            break;
+        case 4:
+            self.lblWelcome.text = @"Cuatro";
+            self.lblWelcome.textColor=[UIColor blackColor];
+            break;
+        case 5:
+            self.lblWelcome.text = @"Cinco";
+            self.lblWelcome.textColor=[UIColor grayColor];
+            break;
+        case 6:
+            self.lblWelcome.text = @"Da click en el boton para iniciar";
+            self.lblWelcome.textColor=[UIColor redColor];
+            x=1;
+            break;
+            
+        default:
+            x=1;
+        break;
+    }
+    x++;
+    
 }
 
 
